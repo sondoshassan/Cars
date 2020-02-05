8a0563fbad67af4a5eabed51437f84925b984fff
sondoshassan <sondos199628@gmail.com>
2020-02-05 09:23:32 +0200

    add some alert

---
+++ js_files/add-content.js
@@ -0,0 +1,9 @@
+var gender = prompt ('enter your gender','female');
+if (gender == 'female') {
+    document.write ('<h3>' + "hi madam" + '</h3>');
+}
+else {
+    document.write ('<h3>' + "hi madam" + '</h3>');
+}
+confirm ("please press button");
+alert ("hello world!");
