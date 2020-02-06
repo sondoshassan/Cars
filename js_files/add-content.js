57c87ac6826adbea42f0d85d440e527851db8888
sondoshassan <sondos199628@gmail.com>
2020-02-06 09:49:10 +0200

    function declration

--- js_files/add-content.js
+++ js_files/add-content.js
@@ -6,6 +6,26 @@ else {
     document.write ('<h3>' + "hi sir" + '</h3>');
 }
 
+function time (){    
+    //input 
+       var hournow = prompt ('enter the time now','it will be 0-24'); //identify var and store the data from prompt 
+        var greeting; //identify var   
+      //process  
+      if (hournow >= 18 && hournow <= 24){ //if statment to determine which massege I want to sent for user      
+          greeting = 'good evening!';    }  
+          else if (hournow >= 12 && hournow < 18){  
+                  greeting = 'good afternoon!';    }  
+              else if (hournow >= 0 && hournow < 12){  
+                      greeting = 'good morning!'; 
+               } 
+                   else {   
+                         greeting = 'welcome!';  
+                  }    //output  
+                  return greeting;
+             }
+            var greeting = time(); //call the function
+            document.write ('<h3>' + greeting + '</h3>');
+            
 var showOrder = function (){
     var orderPhoto = '';
     var order;
