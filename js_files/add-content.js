3fdaccbad9ef534757961de8c55fa18a5556fcb3
sondoshassan <sondos199628@gmail.com>
2020-02-06 09:03:45 +0200

    add prompt

--- js_files/add-content.js
+++ js_files/add-content.js
@@ -5,8 +5,24 @@ if (gender == 'female') {
 else {
     document.write ('<h3>' + "hi sir" + '</h3>');
 }
-prompt ('enter your name');
-var color = prompt ('what do you want the background to be?','ex: black');
-document.body.style.backgroundColor = color;
-confirm ("please press button");
-alert ("hello world!");
+
+var showOrder = function (){
+    var orderPhoto = '';
+    var order;
+    while (order !== "silver" && order !== "black"){
+        order = prompt ("which colors you want","silver or black");
+    }
+    var numberOrder;
+    numberOrder = prompt ("what the numbers of cars you need");
+    for (var i = 0; i < numberOrder; i++){
+        console.log ("i =" + i);
+        if (order == "silver"){
+          orderPhoto = orderPhoto + '<img src="https://inventory-dmg.assets-cdk.com/2/1/0/20465011012.jpg">';
+        }
+        else if (order == "black"){
+            orderPhoto = orderPhoto + '<img src="https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/topics/magazine-article-pool/2018/bmw-m2-coupe-edition-black-shadow/bmw-m2-coup%C3%A9-black-shadow-stage-teaser-si.jpg">';
+        }
+    }
+    return orderPhoto;
+    }
+    document.write (showOrder());
